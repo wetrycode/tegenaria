1d7f62a816d1d9ecc436fd3c91f503776c6ddf71
“qinzhonghe96@gmail.com” <vforfreedom96@gmail.com>
2022-01-22 20:26:40 +0800

    chore:fix timezone error

--- middlewares.go
+++ middlewares.go
@@ -16,3 +16,5 @@ type Middlewares []MiddlewaresInterface
 func (p Middlewares) Len() int           { return len(p) }
 func (p Middlewares) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 func (p Middlewares) Less(i, j int) bool { return p[i].GetPriority() < p[j].GetPriority() }
+
+
