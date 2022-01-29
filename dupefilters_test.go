4391ed163c1194948e897aaa91aee73ea7d00472
qinzhonghe <vforfreedom96@gmail.com>
2022-01-29 17:40:14 +0800

    Release/v0.2.6

--- dupefilters_test.go
+++ dupefilters_test.go
@@ -47,7 +47,6 @@ func TestDoBodyDupeFilter(t *testing.T) {
 	request2 := NewRequest(server.URL+"/testHeader", GET, testParser, RequestWithRequestHeader(headers),RequestWithRequestBody(body))
 	request3 := NewRequest(server.URL+"/testHeader2", GET, testParser, RequestWithRequestHeader(headers))
 	request4 := NewRequest(server.URL+"/testHeader", GET, testParser, RequestWithRequestHeader(headers),RequestWithRequestBody(body))
-
 	duplicates := NewRFPDupeFilter(1024*1024, 5)
 	if r1, err := duplicates.DoDupeFilter(request1); r1||err!=nil {
 		t.Errorf("Request1 error expected=%v, get=%v", false, true)
@@ -64,4 +63,5 @@ func TestDoBodyDupeFilter(t *testing.T) {
 		t.Errorf("Request4 error expected=%v, get=%v", true, false)
 
 	}
+	
 }
