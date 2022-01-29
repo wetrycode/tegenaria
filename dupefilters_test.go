d892dcb537ae0814864093262264300587b0737b
“qinzhonghe96@gmail.com” <vforfreedom96@gmail.com>
2022-01-29 15:35:31 +0800

    fix:unittest cov

--- dupefilters_test.go
+++ dupefilters_test.go
@@ -18,14 +18,50 @@ func TestDoDupeFilter(t *testing.T) {
 
 	duplicates := NewRFPDupeFilter(1024*1024, 5)
 	if r1, _ := duplicates.DoDupeFilter(request1); r1 {
-		t.Errorf("Request1 igerprint sum error expected=%v, get=%v", false, true)
+		t.Errorf("Request1 error expected=%v, get=%v", false, true)
 	}
 	if r2, _ := duplicates.DoDupeFilter(request2); !r2 {
-		t.Errorf("Request2 igerprint sum error expected=%v, get=%v", true, false)
+		t.Errorf("Request2  error expected=%v, get=%v", true, false)
 
 	}
 	if r3, _ := duplicates.DoDupeFilter(request3); r3 {
-		t.Errorf("Request3 igerprint sum error expected=%v, get=%v", false, true)
+		t.Errorf("Request3  error expected=%v, get=%v", false, true)
+
+	}
+}
+
+func TestDoBodyDupeFilter(t *testing.T) {
+	server := newTestServer()
+	// downloader := NewDownloader()
+	headers := map[string]string{
+		"Params1":    "params1",
+		"Intparams":  "1",
+		"Boolparams": "false",
+	}
+	body := make(map[string]interface{})
+	body["test"] = "tegenaria"
+
+	body1 := make(map[string]interface{})
+	body1["test"] = "tegenaria2"
+	request1 := NewRequest(server.URL+"/testHeader", GET, testParser, RequestWithRequestHeader(headers),RequestWithRequestBody(body))
+	request2 := NewRequest(server.URL+"/testHeader", GET, testParser, RequestWithRequestHeader(headers),RequestWithRequestBody(body))
+	request3 := NewRequest(server.URL+"/testHeader2", GET, testParser, RequestWithRequestHeader(headers))
+	request4 := NewRequest(server.URL+"/testHeader", GET, testParser, RequestWithRequestHeader(headers),RequestWithRequestBody(body))
+
+	duplicates := NewRFPDupeFilter(1024*1024, 5)
+	if r1, err := duplicates.DoDupeFilter(request1); r1||err!=nil {
+		t.Errorf("Request1 error expected=%v, get=%v", false, true)
+	}
+	if r2, err := duplicates.DoDupeFilter(request2); !r2||err!=nil {
+		t.Errorf("Request2 error expected=%v, get=%v", true, false)
+
+	}
+	if r3, err := duplicates.DoDupeFilter(request3); r3||err!=nil {
+		t.Errorf("Request3 error expected=%v, get=%v", false, true)
+
+	}
+	if r4, err := duplicates.DoDupeFilter(request4); !r4||err!=nil {
+		t.Errorf("Request4 error expected=%v, get=%v", true, false)
 
 	}
 }
